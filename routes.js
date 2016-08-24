ac74b99c47817f6d2475a2e02e80315abaa5e0e9
oato.o <oat9002@gmail.com>
2016-08-24 15:27:26 +0700

    Edit CORS

--- routes.js
+++ routes.js
@@ -25,7 +25,7 @@ app.use('/twitter', twitterRouter)
 
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:7773')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
